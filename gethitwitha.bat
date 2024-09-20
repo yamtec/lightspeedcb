b4a0091b58ec608bcca497c8eb27950c2afab0a5
yamtec <80838861+yamtec@users.noreply.github.com>
2024-09-20 13:37:41 -0400

    Update and rename lightspeed/killer.bat to gethitwitha.bat

--- gethitwitha.bat
+++ gethitwitha.bat
@@ -6,11 +6,12 @@ SET EXE=ClassroomWindows.exe
 REM for testing
 REM SET EXE=svchost.exe
 FOR /F %%x IN ('tasklist /NH /FI "IMAGENAME eq %EXE%"') DO IF NOT %%x == %EXE% (
-  ECHO %EXE% is Not Running
+  ECHO %EXE% is Not Running - Made by Combobreak
+ECHO %EXE% Kill attemped made.
   REM This GOTO may be not necessary
   GOTO notRunning
 ) ELSE (
-  ECHO %EXE% is running
+  ECHO %EXE% is running - Made By Combobreak
   GOTO Running
 )
 :Running
